721cc52b0ec8ad71e2191f3082e04eced36f43db
irsyadagungr <87929601+irsyadagungr@users.noreply.github.com>
2025-03-27 20:35:52 +0700

    esp_app.py

--- esp_app.py
+++ esp_app.py
@@ -118,15 +118,15 @@ if result:
 
     #MODEL INITIALIZE
     #Get the absolute path of the current script
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    model_path = os.path.join(base_dir, "my_model.h5")
+    #base_dir = os.path.dirname(os.path.abspath(__file__))
+    #model_path = os.path.join(base_dir, "my_model.h5")
     
     #Load the model
-    if os.path.exists(model_path):
-        new_model = tf.keras.models.load_model(model_path)
-        print("✅ Model loaded successfully in Streamlit!")
-    else:
-        raise FileNotFoundError(f"❌ Model file NOT found: {model_path}")
+    #if os.path.exists(model_path):
+        #new_model = tf.keras.models.load_model(model_path)
+        #print("✅ Model loaded successfully in Streamlit!")
+    #else:
+        #raise FileNotFoundError(f"❌ Model file NOT found: {model_path}")
     
     #model_path = "my_model.h5"  # New model file
 
@@ -139,16 +139,16 @@ if result:
 
     #new_model = tf.keras.models.load_model('saved_modelik1/my_modelik1')
     #Get absolute path
-    #model_path = os.path.join(os.path.dirname(__file__), "saved_modelik1", "my_modelik1")
+    model_path = os.path.join(os.path.dirname(__file__), "saved_modelik1", "my_modelik1")
 
-    # Debug print to check if the path exists
-    #if os.path.exists(model_path):
-        #print(f"✅ Model found at: {model_path}")
-    #else:
-        #raise FileNotFoundError(f"❌ Model NOT found at: {model_path}")
+    Debug print to check if the path exists
+    if os.path.exists(model_path):
+        print(f"✅ Model found at: {model_path}")
+    else:
+        raise FileNotFoundError(f"❌ Model NOT found at: {model_path}")
 
-    # Load the model
-    #new_model = tf.keras.models.load_model(model_path)
+    #Load the model
+    new_model = tf.keras.models.load_model(model_path)
 
     #DATASET BARU
     def create_dataset(X, time_steps=1, step=1):
